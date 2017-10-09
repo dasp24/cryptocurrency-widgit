c495c3acf2964525c778135dbb3533bdde2653f5
Dan Parkes <daniel.parkes901@gmail.com>
2017-10-09 13:51:04 +0100

    removed redux components as its no longer necessry

--- src/redux/currencies/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const RECEIVING_BITCOIN = 'currencies/RECEIVING_BITCOIN';
-export const RECEIVING_ETHEREUM = 'currencies/RECEIVING_ETHEREUM';
-export const RECEIVING_LITECOIN = 'currencies/RECEIVING_LITECOIN';
-export const RECEIVING_EXCHANGE = 'currencies/RECEIVING_EXCHANGE';
-export const RECEIVING_RIPPLE = 'currencies/RECEIVING_RIPPLE';
