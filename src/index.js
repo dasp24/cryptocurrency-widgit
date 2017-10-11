cede2f2608d0de9e260731411f00c874160c49be
Dan Parkes <daniel.parkes901@gmail.com>
2017-10-11 11:17:53 +0100

    removed redux and app component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 
 import './index.css';
 import Cryptocurrency from './Cryptocurrency';
-import CreateStore from './redux/store';
+
 import registerServiceWorker from './registerServiceWorker';
 
-const App = () => {
-    return (
-      <Provider store={CreateStore()}>
-          <Cryptocurrency/>
-      </Provider>);
-  };
 
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Cryptocurrency/>, document.getElementById('root'));
 registerServiceWorker();
