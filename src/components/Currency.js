6988ab91f9142e66d2a046e5bdceebf254604722
Dan Parkes <daniel.parkes901@gmail.com>
2017-10-11 12:10:33 +0100

    changed to receive new props format

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -7,14 +7,16 @@ class Currency extends React.Component {
         this.props.handleInputChange(name,value);
     }
     render() {
+        const data = this.props.data;
+        const exchangeRate = this.props.exchangeRate;
         return (
-                <tr id={this.props.id}>
-                  <td>{this.props.coin}</td>
-                  <td className="numbers"> {this.props.price / this.props.exchangeRate ? (this.props.price / this.props.exchangeRate).toFixed(4) : 'loading'}</td>
-                  <td style={(this.props.changeInDay > 0) ? {backgroundColor: '#2db300'} : {backgroundColor: '#ff704d'}}>{this.props.changeInDay}%{(this.props.changeInDay > 0) ? <a className="arrows">⬆️</a> : <a className="arrows">⬇️</a>}</td>
-                  <td><input ref={ref => this.inputRef = ref} style={{border:'none'}} type="integer" name={this.props.coin} placeholder="enter value..." onInput={this.onInput.bind(this)}/></td>
-                  <td className="numbers">£{(this.props.state[this.props.coin] * this.props.price / this.props.exchangeRate).toFixed(2)}</td>     
-                  <td><button className="table_buttons" onClick={() => this.props.removeCoin(this.props.id)}>remove</button></td>    
+                <tr id={data.id}>
+                  <td className="name">{data.name}</td>
+                  <td className="numbers"> {data.price / exchangeRate ? (data.price / exchangeRate).toFixed(4) : 'loading'}</td>
+                  <td style={(data.changeInDay > 0) ? {backgroundColor: '#2db300'} : {backgroundColor: '#ff704d'}}>{data.changeInDay}%{(data.changeInDay > 0) ? <a className="arrows">⬆️</a> : <a className="arrows">⬇️</a>}</td>
+                  <td><input className="input" ref={ref => this.inputRef = ref} style={{border:'none'}} type="integer" name={data.name} placeholder="enter value..." onInput={this.onInput.bind(this)}/></td>
+                  <td className="numbers">£{this.props.state[data.name] ? (this.props.state[data.name] * data.price / exchangeRate).toFixed(2) : 0}</td>     
+                  <td><button className="table_buttons" onClick={() => this.props.removeCoin(data.id,data.name)}>remove</button></td>    
                 </tr>
         );
   }
