d31d6619f9375b7c6ec5e9021379d2fa10077646
Dan Parkes <daniel.parkes901@gmail.com>
2017-10-12 14:06:39 +0100

    added functionality when enter is pressed

--- src/components/NewFeed.js
+++ src/components/NewFeed.js
@@ -4,7 +4,10 @@ class NewFeed extends React.Component {
     render() {
         return (
             <div className="input_twitter">
-            <input ref={ref => this.twitterInputRef = ref}  placeholder="Add twitter ID...."/>
+            <input onKeyPress={(e) => {if (e.key === 'Enter') {
+                      this.props.addFeed(this.twitterInputRef.value);
+                      this.twitterInputRef.value = null;
+                  }}} ref={ref => this.twitterInputRef = ref}  placeholder="Add twitter ID...."/>
             <button className="input_buttons" onClick={() => {
                 this.props.addFeed(this.twitterInputRef.value);
                 this.twitterInputRef.value = null;
