6dbabfbc7b108e9d698772f8d32823320a7fb201
Dan Parkes <daniel.parkes901@gmail.com>
2017-10-12 14:05:24 +0100

    added linting disable

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
+/* eslint-disable no-unused-vars*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
 import Cryptocurrency from './Cryptocurrency';
-
 import registerServiceWorker from './registerServiceWorker';
-
+/* eslint-enable no-unused-vars*/
 
 ReactDOM.render(<Cryptocurrency/>, document.getElementById('root'));
 registerServiceWorker();
