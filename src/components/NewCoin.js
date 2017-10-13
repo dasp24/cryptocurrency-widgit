d08d695fb8741680faadc0a83432262e523ab9b6
Dan Parkes <daniel.parkes901@gmail.com>
2017-10-13 10:31:49 +0100

    changed placeholder as it now accepts name and coin

--- src/components/NewCoin.js
+++ src/components/NewCoin.js
@@ -7,7 +7,7 @@ class NewCoin extends React.Component {
                   <input onKeyPress={(e) => {if (e.key === 'Enter') {
                       this.props.addCoin(this.currencyInputRef.value);
                       this.currencyInputRef.value = null;
-                  }}} ref={ref => this.currencyInputRef = ref}  placeholder="Insert coin ID here..."/>
+                  }}} ref={ref => this.currencyInputRef = ref}  placeholder="Add coin..."/>
                   <button className="input_buttons" onClick={() => {
                       this.props.addCoin(this.currencyInputRef.value);
                       this.currencyInputRef.value = null;
