d9fc18757b6c7c15d40da89a65dfb22e7e2cf6a8
Dan Parkes <daniel.parkes901@gmail.com>
2017-10-15 00:50:53 +0100

    new coin is now the bottom of table with a plus char instead of button

--- src/components/NewCoin.js
+++ src/components/NewCoin.js
@@ -3,16 +3,20 @@ import React from 'react';
 class NewCoin extends React.Component {
     render() {
         return (
-            <div>
-                  <input onKeyPress={(e) => {if (e.key === 'Enter') {
+            <tr>
+                 <td> <input className="table_buttons" onKeyPress={(e) => {if (e.key === 'Enter') {
                       this.props.addCoin(this.currencyInputRef.value);
                       this.currencyInputRef.value = null;
-                  }}} ref={ref => this.currencyInputRef = ref}  placeholder="Add coin..."/>
-                  <button className="input_buttons" onClick={() => {
+                  }}} ref={ref => this.currencyInputRef = ref}  placeholder="Add coin..."/></td>
+                 <td><i onClick={() => {
                       this.props.addCoin(this.currencyInputRef.value);
                       this.currencyInputRef.value = null;
-                    }}>Add Coin</button>
-            </div>
+                    }} className="fa fa-plus-square" aria-hidden="true"></i></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+            </tr>
         );
   }
 }
