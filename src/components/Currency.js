90b6f118fd8389e6e7f6f33f1049751839e864d0
Dan Parkes <daniel.parkes901@gmail.com>
2017-10-15 00:50:14 +0100

    changed input field to edit and ask for amount

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,22 +1,17 @@
 import React from 'react';
 
 class Currency extends React.Component {
-    onInput() {
-        const name = this.inputRef.name;
-        const value = this.inputRef.value;
-        this.props.handleInputChange(name,value);
-    }
     render() {
-        const data = this.props.data;
-        const exchangeRate = this.props.exchangeRate;
+        const {data,exchangeRate,worth} = this.props;
+
         return (
                 <tr id={data.id}>
                   <td className="name">{data.name}</td>
                   <td className="numbers"> {data.price / exchangeRate ? (data.price / exchangeRate).toFixed(4) : 'loading'}</td>
                   <td style={(data.changeInDay > 0) ? {backgroundColor: '#2db300'} : {backgroundColor: '#ff704d'}}>{data.changeInDay}%{(data.changeInDay > 0) ? <a className="arrows">⬆️</a> : <a className="arrows">⬇️</a>}</td>
-                  <td><input className="input" ref={ref => this.inputRef = ref} style={{border:'none'}} type="integer" name={data.name} placeholder="enter value..." onInput={this.onInput.bind(this)}/></td>
-                  <td className="numbers">£{this.props.state[data.name] ? (this.props.state[data.name] * data.price / exchangeRate).toFixed(2) : 0}</td>     
-                  <td><button className="table_buttons" onClick={() => this.props.removeCoin(data.id,data.name)}>remove</button></td>    
+                  <td>{worth[data.name]}<i onClick={() => this.props.editValue(data.id,data.name)} className="fa fa-pencil" aria-hidden="true">></i></td>
+                  <td className="numbers">£{worth[data.name] ? (worth[data.name] * data.price / exchangeRate).toFixed(2) : 0}</td>     
+                  <td><i onClick={() => this.props.removeCoin(data.id,data.name)}className ="fa fa-trash table_buttons" aria-hidden="true"></i></td>    
                 </tr>
         );
   }
