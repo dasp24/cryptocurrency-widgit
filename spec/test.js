7ed69b9c042a19914c947437e65b3d6c9abcce34
Dan Parkes <daniel.parkes901@gmail.com>
2017-10-23 17:47:13 +0100

    added some new tests

--- spec/test.js
+++ spec/test.js
@@ -1,8 +1,8 @@
 const fetch = require('node-fetch');
 
-
-
-const obj = {currencies:{}}
+const obj = {
+    currencies: {}
+};
 
 const getStuff = (id) => fetch(`http://coincap.io/page/${id}`)
     .then((response) => response.json())
@@ -14,28 +14,47 @@ const getStuff = (id) => fetch(`http://coincap.io/page/${id}`)
                 changeInDay: data.cap24hrChange,
                 id: data.id
             }
-        }
-        return obj
-    })
-const getStuffInState = (id) => fetch(`http://coincap.io/page/${id}`)
-    .then((response) => response.json())
-    .then((data) => {
-        obj.currencies[data.display_name] = {
-                name: data.display_name,
-                price: data.price_usd,
-                changeInDay: data.cap24hrChange,
-                id: data.id
         };
         return obj;
     });
+const getStuffInState = (id) => fetch(`https://api.coinmarketcap.com/v1/ticker/${id}/?convert=GBP`,{cache: 'no-store'})
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        obj.currencies[data[0].name] = {
+            name: data[0].name,
+            price: data[0].price_gbp,
+            changeInDay: data[0].percent_change_24h,
+            id: data[0].symbol
+        };
+        return obj;
+    })
+    .then(data=>console.log(data));
 
-const getIt = (a,b) => Promise.all([getStuffInState(a),
-        getStuffInState(b)])
-        .then(()=>console.log(obj))
-   
-getIt('XRP','BTC');
+    getStuffInState('bitcoin');
+
+const getIt = (a, b) => Promise.all([getStuffInState(a),
+        getStuffInState(b)
+    ])
+    .then(() => console.log(obj))
+
+    const obj2={}
+const allNamesAndIds = () => fetch('http://coincap.io/coins')
+    .then((response) => response.json())
+    .then((data) => {
+        Promise.all(data.forEach((id) => {
+        fetch(`http://coincap.io/page/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                obj2[data.display_name] = data.id;
+            });
+    }))
+    .then(()=> obj2)
+    })
+    .then((data) => console.log(data));
 
 module.exports = {
     getStuff,
-    getStuffInState
+    getStuffInState,
+    getIt
 };
