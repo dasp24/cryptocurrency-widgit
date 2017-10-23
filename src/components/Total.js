896fdb5739484d459209ffcb3c563ee24d7e9c26
Dan Parkes <daniel.parkes901@gmail.com>
2017-10-23 17:46:42 +0100

    removed exchange rate

--- src/components/Total.js
+++ src/components/Total.js
@@ -3,10 +3,10 @@ import _ from 'underscore';
 
 class Total extends React.Component {
     render() {
-        const {currencies, exchangeRate, worth} = this.props;
+        const {currencies, worth} = this.props;
         return (
-                <div className="numbers">£{currencies ? _.reduce(currencies,(acc,coin) => {
-                    acc += (Number(coin.price) / Number(exchangeRate)) * Number(worth[coin.name]||0); 
+                <div className="numbers">Total Portfolio Worth: £{currencies ? _.reduce(currencies,(acc,coin) => {
+                    acc += Number(coin.price) * Number(worth[coin.name] || 0); 
                     return acc;                            
                   },0).toFixed(2) : null}
                 </div>
