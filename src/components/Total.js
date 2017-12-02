954b132e3a36d1f5db093c11ec47af397e27ff67
Dan Parkes <daniel.parkes901@gmail.com>
2017-12-02 23:51:36 +0000

    moved class name and replace with id to centre text on page

--- src/components/Total.js
+++ src/components/Total.js
@@ -5,7 +5,7 @@ class Total extends React.Component {
     render() {
         const {currencies, worth} = this.props;
         return (
-                <div className="numbers">Total Portfolio Worth: £{currencies ? _.reduce(currencies,(acc,coin) => {
+                <div id="total">Total Portfolio Worth: £{currencies ? _.reduce(currencies,(acc,coin) => {
                     acc += Number(coin.price) * Number(worth[coin.name] || 0); 
                     return acc;                            
                   },0).toFixed(2) : null}
