2128fbd725df6f2344cbf546f5595fa9f82cc189
Dan Parkes <daniel.parkes901@gmail.com>
2017-12-02 23:51:04 +0000

    removed extra >

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -10,7 +10,7 @@ class Currency extends React.Component {
                   <td className="numbers"> {data.price  ? Number(data.price).toFixed(4) : 'loading'}</td>
                   <td style={(data.changeInDay > 0) ? {backgroundColor: '#2db300'} : {backgroundColor: '#ff704d'}}>{data.changeInDay}%{(data.changeInDay > 0) ? <i className="fa fa-arrow-up" aria-hidden="true"></i> : <i className="fa fa-arrow-down" aria-hidden="true"></i>}</td>
                   <td>{worth[data.name]}</td>
-                  <td><i onClick={() => this.props.editValue(data.id,data.name)} className="fa fa-pencil" aria-hidden="true">></i></td>
+                  <td><i onClick={() => this.props.editValue(data.id,data.name)} className="fa fa-pencil" aria-hidden="true"></i></td>
                   <td className="numbers">£{worth[data.name] ? (worth[data.name] * data.price).toFixed(2) : 0}</td>     
                   <td><i onClick={() => this.props.removeCoin(data.name)}className ="fa fa-trash table_buttons" aria-hidden="true"></i></td>    
                 </tr>
